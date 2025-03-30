30353e44208816c626232e7cd445c17cb241e6ca
Holthuizen <holthuizen2@gmail.com>
2025-03-30 19:57:56 +0200

    game with udp trigger logic on mouse click

--- game_udp.py
+++ game_udp.py
@@ -4,6 +4,10 @@ import noise
 import numpy as np
 import colorsys
 
+import socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+addr = ("127.0.0.1", 1000)
+
 FPS = 24
 W,H = 600, 800
 R = 200
@@ -14,6 +18,14 @@ screen = pygame.display.set_mode((W,H)) #with height
 clock = pygame.time.Clock()
 color_update_speed = 0.8
 angle = 0
+
+
+def send_bytes(msg, socket, endpoint):
+    if len(msg) > 0: 
+        bmsg = bytes(msg)
+        sock.sendto(bmsg, endpoint)
+    
+
 
 #fixes the center issue and provides anti-aliasing  
 def draw_circle(surface, x, y, radius, color):
@@ -45,16 +57,16 @@ while RUNNING:
     cc = angle_to_rgb(angle) #circle color
     draw_circle(screen,round(W/2), round(H/2), R, cc)
 
-
-    x, y = pygame.mouse.get_pos()
-
     #events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             RUNNING = False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            print("Mouse clicked at:", x, y)
+            sock.close()
 
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = pygame.mouse.get_pos()  # Get position when clicked
+            print("Mouse clicked at:", x, y)
+            send_bytes([1], socket=sock, endpoint=addr) #sends the int 1
 
     #draw next frame
     pygame.display.flip() 
